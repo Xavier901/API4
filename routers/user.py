966beeeda3a3dec3e217b829dfdee7a5d965c4cc
Xavier901 <39602489+Xavier901@users.noreply.github.com>
2025-04-16 16:52:01 +0530

    user creation

--- routers/user.py
+++ routers/user.py
@@ -13,7 +13,13 @@ router=APIRouter(
 
 
 
-@router.get('/create_user')
-def create_user():
-    return{"user":"user"}
+@router.post('/create_user')
+def create_user(user:schemas.UserCreate,db:Session=Depends(get_db)):
+    
+    new_user=models.User(**user.dict()) # get user query from schemas data
+    db.add(new_user)                    #add it to database
+    db.commit()                         #save changes to database
+    db.refresh(new_user)                #update the new user
+    
+    return new_user                     #return the new user
 
