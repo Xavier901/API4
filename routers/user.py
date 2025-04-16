ffd4f4b8169b5eda3567cad356628b0a2df325d3
Xavier901 <39602489+Xavier901@users.noreply.github.com>
2025-04-16 17:03:26 +0530

    get user by id

--- routers/user.py
+++ routers/user.py
@@ -14,7 +14,7 @@ router=APIRouter(
 
 
 @router.post('/create_user')
-def create_user(user:schemas.UserCreate,db:Session=Depends(get_db)):
+def create_user(user:schemas.UserCreate,db:Session=Depends(get_db)):  #taking query as schema #connection to database
     
     new_user=models.User(**user.dict()) # get user query from schemas data
     db.add(new_user)                    #add it to database
@@ -23,3 +23,14 @@ def create_user(user:schemas.UserCreate,db:Session=Depends(get_db)):
     
     return new_user                     #return the new user
 
+
+
+@router.get('/{id}',response_model=schemas.UserOut)  #pathway ,# responce based userout
+def get_user(id:int,db:Session=Depends(get_db)):  #grab data from database
+    
+    user=db.query(models.User).filter(models.User.id==id).first()  #find the id base user
+    
+    if not user:                                                   #if id not found raise exception
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id:{id} doesn't exist.")
+    return user                                                     # return user data
